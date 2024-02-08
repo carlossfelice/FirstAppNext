7f40fdbf11992206b0fd74298853a48dfb613a1e
unknown <carlosfelicelxn@gmail.com>
2024-02-08 17:22:56 -0300

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio quae doloribus sit quisquam. Fugiat suscipit qui similique sunt a molestias earum nostrum, ducimus fugit fuga atque praesentium eveniet aliquid molestiae?</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/app/services/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ServicePage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ab non deserunt officiis numquam? Ab, aut nesciunt accusamus nam itaque voluptatum ut repellendus minima? Molestiae laborum, sapiente, quod doloremque distinctio commodi, iusto recusandae eos ab quisquam amet. Tempore molestiae illum aspernatur iure, asperiores, facilis tempora eius maiores dolorem natus, et sed quo ipsam consequuntur odit reprehenderit nesciunt quibusdam officiis.</p>
+    </div>
+  )
+}
+
+export default ServicePage
